19ee63d33a25d48c2307ffe04e176dd5ec0dfe8f
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-02-14 10:56:39 +0700

    feat: add calendar github & customize scrollbar

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -16,6 +16,7 @@ import { RiTailwindCssFill } from "react-icons/ri";
 import { IoLogoJavascript } from "react-icons/io";
 import { SiJest, SiMysql, SiGooglecloud, SiWoocommerce } from "react-icons/si";
 import { motion } from "framer-motion";
+import GitHubCalendar from "react-github-calendar";
 
 const iconMap = {
   FaReact: <FaReact className="text-cyan-400" />,
@@ -38,80 +39,99 @@ const iconMap = {
 
 const Experience = () => {
   return (
-    <div id="experience" className="py-10 px-4 sm:px-8 md:px-16 lg:px-32">
-      <motion.h2
-        whileInView={{ opacity: 1, y: 0 }}
-        initial={{ opacity: 0, y: -100 }}
-        transition={{ duration: 1 }}
-        className="my-16 text-center text-3xl lg:text-4xl font-bold bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text tracking-tight text-transparent"
-      >
-        Experience
-      </motion.h2>
-      <div className="relative">
-        {/* Timeline line */}
-        <div className="absolute left-1/2 transform -translate-x-1/2 h-full w-px bg-stone-700" />
+    <>
+      <div id="experience" className="py-10 px-4 sm:px-8 md:px-16 lg:px-32">
+        <motion.h2
+          whileInView={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: -100 }}
+          transition={{ duration: 1 }}
+          className="my-16 text-center text-3xl lg:text-4xl font-bold bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text tracking-tight text-transparent"
+        >
+          Experience
+        </motion.h2>
+        <div className="relative">
+          {/* Timeline line */}
+          <div className="absolute left-1/2 transform -translate-x-1/2 h-full w-px bg-stone-700" />
 
-        {EXPERIENCES.map((experience) => (
-          <div
-            key={experience.id}
-            className="mb-12 flex flex-wrap lg:flex-nowrap relative items-start"
-          >
-            {/* Timeline dot */}
-            <motion.div className="absolute left-1/2 transform -translate-x-1/2 top-[24px] lg:top-0  w-4 h-4 bg-stone-600 rounded-full border-4 border-stone-800" />
-
-            {/* Year section */}
-            <motion.div
-              whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: 100 }}
-              transition={{ duration: 1 }}
-              className="w-full lg:w-1/2 lg:pr-16 text-center lg:text-right"
+          {EXPERIENCES.map((experience) => (
+            <div
+              key={experience.id}
+              className="mb-12 flex flex-wrap lg:flex-nowrap relative items-start"
             >
-              <p className="text-sm text-stone-400 font-medium">
-                {experience.year}
-              </p>
-            </motion.div>
+              {/* Timeline dot */}
+              <motion.div className="absolute left-1/2 transform -translate-x-1/2 top-[24px] lg:top-0  w-4 h-4 bg-stone-600 rounded-full border-4 border-stone-800" />
 
-            {/* Content section */}
-            <div className="w-full lg:w-1/2 lg:pl-16 mt-6 lg:mt-0">
-              <div className="mb-4">
-                <motion.div
-                  whileInView={{ opacity: 1, x: 0 }}
-                  initial={{ opacity: 0, x: -100 }}
-                  transition={{ duration: 1 }}
-                  className="mb-2 font-semibold text-stone-800 dark:text-white"
-                >
-                  {experience.role} -{" "}
-                  <span className="text-sm text-stone-400">
-                    {experience.company}
-                  </span>
-                </motion.div>
-                <motion.p
-                  whileInView={{ opacity: 1, x: 0 }}
-                  initial={{ opacity: 0, x: -100 }}
-                  transition={{ duration: 1 }}
-                  className="mb-4 text-stone-400"
-                >
-                  {experience.description}
-                </motion.p>
-                <div className="flex flex-wrap gap-2">
-                  {experience.technologies.map((tech) => (
-                    <motion.span
-                      whileInView={{ opacity: 1, x: 0 }}
-                      initial={{ opacity: 0, x: -100 }}
-                      transition={{ duration: 1 }}
-                      key={tech.id}
-                      className="flex flex-row items-center gap-2 rounded-full bg-stone-700 px-3 py-1 text-sm font-semibold text-white"
-                    >
-                      {iconMap[tech.icon]} {tech.nama}
-                    </motion.span>
-                  ))}
+              {/* Year section */}
+              <motion.div
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: 100 }}
+                transition={{ duration: 1 }}
+                className="w-full lg:w-1/2 lg:pr-16 text-center lg:text-right"
+              >
+                <p className="text-sm text-stone-400 font-medium">
+                  {experience.year}
+                </p>
+              </motion.div>
+
+              {/* Content section */}
+              <div className="w-full lg:w-1/2 lg:pl-16 mt-6 lg:mt-0">
+                <div className="mb-4">
+                  <motion.div
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: -100 }}
+                    transition={{ duration: 1 }}
+                    className="mb-2 font-semibold text-stone-800 dark:text-white"
+                  >
+                    {experience.role} -{" "}
+                    <span className="text-sm text-stone-400">
+                      {experience.company}
+                    </span>
+                  </motion.div>
+                  <motion.p
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: -100 }}
+                    transition={{ duration: 1 }}
+                    className="mb-4 text-stone-400"
+                  >
+                    {experience.description}
+                  </motion.p>
+                  <div className="flex flex-wrap gap-2">
+                    {experience.technologies.map((tech) => (
+                      <motion.span
+                        whileInView={{ opacity: 1, x: 0 }}
+                        initial={{ opacity: 0, x: -100 }}
+                        transition={{ duration: 1 }}
+                        key={tech.id}
+                        className="flex flex-row items-center gap-2 rounded-full bg-stone-700 px-3 py-1 text-sm font-semibold text-white"
+                      >
+                        {iconMap[tech.icon]} {tech.nama}
+                      </motion.span>
+                    ))}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+
+      <div className="py-10 flex flex-col items-center px-4 sm:px-8 md:px-16 lg:px-32">
+        <motion.h2
+          whileInView={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: -100 }}
+          transition={{ duration: 1 }}
+          className="my-16 text-center text-3xl lg:text-4xl font-bold bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text tracking-tight text-transparent"
+        >
+          Github Contributions
+        </motion.h2>
+        <GitHubCalendar
+          username="rahmadfahrurrozi"
+          blockSize={12}
+          blockMargin={5}
+          fontSize={12}
+        />
+      </div>
+    </>
   );
 };
 
